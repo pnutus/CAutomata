efebd2af969356866bb4180aea2f4a29d0b4666c
Pontus Granström <pnutus@gmail.com>
2015-02-13 23:50:11 +0100

    Initial Commit

---
+++ CellularAutomata/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CellularAutomata
+//
+//  Created by Pontus Granström on 2015-02-13.
+//  Copyright (c) 2015 Pontus Granström. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
