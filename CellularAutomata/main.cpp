6ce4bf5340c127b2dd33b9fb53510830be047f5e
Pontus Granström <pnutus@gmail.com>
2015-03-09 00:24:44 +0100

    Arbitrary-length states

--- CellularAutomata/main.cpp
+++ CellularAutomata/main.cpp
@@ -9,71 +9,143 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include "pcg_basic.h"
 
+typedef uint32_t u32;
 typedef int64_t  s64;
 typedef uint64_t u64;
 typedef uint8_t u8;
 typedef uint32_t bool32;
 
-// TODO(Pontus): States of arbitrary length
+#define CHUNK_SIZE 64
 
-void print_state(u64 state)
+struct ca_state {
+    int bit_count;
+    u64* bits;
+};
+
+void print_state(ca_state state)
 {
-    char string[65] = {};
-    for (int digit_index = 63;
-         digit_index >= 0;
-         digit_index--)
+    // TODO: Make this two loops instead of divmod every digit
+    char string[state.bit_count + 1];
+    for (int bit_index = 0;
+         bit_index < state.bit_count;
+         bit_index++)
     {
-        bool32 active = ((state >> digit_index) & 1);
+        u64 state_chunk = state.bits[bit_index / CHUNK_SIZE];
+        int chunk_index = bit_index % CHUNK_SIZE;
+        bool32 active = ((state_chunk >> chunk_index) & 1);
         char character = active * '#' + (1 - active) * ' ';
-        string[digit_index] = character;
+        string[state.bit_count - bit_index - 1] = character;
     }
+    string[state.bit_count] = 0;
     printf("%s\n", string);
 }
 
-void ca(u8 rule, u64 initial_state)
+int string_length(const char* string)
 {
-    u64 parent_state = initial_state;
-    u8 three_mask = 7; // 111
-    for (;;)
+    int result = 0;
+    while (*string++)
     {
-        print_state(parent_state);
-        usleep(50000);
-        
-        u64 state = 0;
-        for (int cell_index = 0;
-             cell_index < 62;
-             cell_index++)
-        {
-            u8 three_state = (parent_state >> cell_index) & three_mask;
-            u64 active = ((rule >> three_state) & 1);
-            state |= (active << (cell_index + 1));
-        }
-        
-        u64 edges = ( ((random() & 1) << 63) | (random() & 1) );
-        parent_state = state | edges;
+        result++;
     }
-    return;
+    return result;
+}
+
+ca_state init_state(int bit_count)
+{
+    ca_state state;
+    state.bit_count = bit_count;
+    state.bits = (u64*) malloc((state.bit_count + 64) / 8);
+    return state;
+}
+
+
+ca_state state_from_string(const char* string)
+{
+    ca_state state = init_state(string_length(string));
+    for (int bit_index = 0;
+         bit_index < state.bit_count;
+         bit_index++)
+    {
+        u64 chunk = state.bits[bit_index / CHUNK_SIZE];
+        chunk = (chunk << 1) | (string[bit_index] != ' ');
+        state.bits[bit_index / CHUNK_SIZE] = chunk;
+    }
+    return state;
+}
+
+ca_state evolve_ca(ca_state parent_state, u8 rule, ca_state child_state)
+{
+    memset(child_state.bits, 0, (child_state.bit_count + 64) / 8);
+    u64 right_edge = (pcg32_random() & 1);
+    child_state.bits[0] = right_edge;
+    
+    u32* parent_half_chunks = (u32*) parent_state.bits;
+    u32* child_half_chunks  = (u32*) child_state.bits;
+    
+    for (int half_chunk_index = 0;
+         half_chunk_index < ((parent_state.bit_count / (CHUNK_SIZE/2)) + 1);
+         half_chunk_index++)
+    {
+        u64 parent_chunk = *((u64*) (parent_half_chunks++));
+        u64 child_chunk = 0;
+        
+        for (int bit_index = 0;
+             bit_index < (CHUNK_SIZE / 2);
+             bit_index++)
+        {
+            if ((half_chunk_index*CHUNK_SIZE/2 + bit_index + 2) >= parent_state.bit_count)
+            {
+                u64 left_edge = (pcg32_random() & 1) << (bit_index + 1);
+                child_chunk |= left_edge;
+                break;
+            }
+            
+            u8 three_mask = 7; // 111
+            u8 three_state = (parent_chunk >> bit_index) & three_mask;
+            u64 active = ((rule >> three_state) & 1);
+            child_chunk |= (active << (bit_index + 1));
+        }
+        u64* child_chunk_pointer = (u64*) (child_half_chunks++);
+        *child_chunk_pointer |= child_chunk;
+        
+    }
+    return child_state;
 }
 
 int main(int argc, const char * argv[]) {
     if (argc > 1)
     {
-        srandomdev();
-        u64 initial_state;
+        pcg32_srandom(time(0), 0);
+        
         u8 rule = atoi(argv[1]);
+        printf("R%d\n", rule);
+        
+        ca_state state;
         if (argc > 2)
         {
-            //Todo(Pontus): Parse this string as a bit string instead
-            initial_state = atoll(argv[2]);
+            state = state_from_string(argv[2]);
         }
         else
         {
-            initial_state = (random() << 32) | random();
+            state = init_state(32);
+            state.bits[0] = pcg32_random();
         }
-        printf("R%d\n", rule);
-        ca(rule, initial_state);
+        
+        ca_state new_state = init_state(state.bit_count);
+        
+        for (;;)
+        {
+            print_state(state);
+            evolve_ca(state, rule, new_state);
+            
+            ca_state tmp = state;
+            state = new_state;
+            new_state = tmp;
+        }
     }
     else
     {
